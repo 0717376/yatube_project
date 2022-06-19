15772ae1b9ab609a3c90c8dceb72b41b961fbe9a
Sergey Muravskiy <0717376@gmail.com>
2022-06-19 09:39:51 +0300

    Добоавлены две модели - Post и Group

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+app_name = 'posts'
 
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name='index'),
     # Страница со списком сообществ
-    path('group/<slug:slug>/', views.group_posts)
+    path('groups/<slug:slug>/', views.group_posts, name='group_list'),
 ]
